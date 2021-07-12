a4859663ac72a823ba33808fa5e0484f0f0a68ca
SinanKulen <sinan.kulen@gmail.com>
2021-07-13 01:39:04 +0300

    GestureRecognizer

--- GestureRecognizer/ViewController.swift
+++ GestureRecognizer/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var ikion = true
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let    gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changepic))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
 
-
+    @objc func changepic() {
+    
+        if ikion == true {
+        imageView.image = UIImage(named: "ikiyeon")
+        myLabel.text = "ikiyeon"
+        ikion = false
+    }
+        else {
+        imageView.image = UIImage(named: "ikiyeon2")
+        myLabel.text = "ikiyeon2"
+        ikion = true
+    }
+    
 }
 
+}
